20084a47d47722c7b97553fb73b7df641a815f26
Julian Meyer <julianmeyer2000@gmail.com>
2019-03-21 16:39:40 -0700

    Fix bug with bad gateway

--- main.go
+++ main.go
@@ -77,6 +77,7 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Server Error", http.StatusBadGateway)
 		log.Printf("error: %s", err)
+		return
 	}
 
 	defer resp.Body.Close()
